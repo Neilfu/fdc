94b9f9a2519107b53c1e67dcdd32155230f9377a
Neilfu <go2pkumba@gmail.com>
2013-10-11 22:59:22 +0800

    Update fdcData.py

--- fdcData.py
+++ fdcData.py
@@ -6,7 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-#modify by neilfu 2013-10-11
+#modify by neilfu  2013-10-12
 #add a line from gitserver
 def getDetail(url):
     mapDetail={}
